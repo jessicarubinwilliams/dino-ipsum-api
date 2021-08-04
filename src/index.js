560ac847ee8baf82452cf63ede2935aca756ace1
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-03 20:59:51 -0700

    add user interface logic
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DinoService from './dino-service.js';
+
+$("#formOne").submit(function() {
+  event.preventDefault();
+  const words = $("#inputtedWords").val();
+  const paragraphs = $("#inputtedParagraphs").val();
+  let promise = DinoService.getDinos(words, paragraphs);
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+    $("#showDinos").text(body);
+  }, function(error) {
+    $(".showErrors").text(`A comet wiped out your dinosaurs. The comet says" ${error}`);
+  });
+});
