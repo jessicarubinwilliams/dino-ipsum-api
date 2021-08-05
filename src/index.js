ae9f97789c182b186d222166b2ea54288b9167c8
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-04 20:09:11 -0700

    update uil to call counter & translator functions
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import DinoService from './dino-service.js';
+import Translation from './translator.js';
 
 $("#formOne").submit(function() {
   event.preventDefault();
-  const words = $("#inputtedWords").val();
-  const paragraphs = $("#inputtedParagraphs").val();
-  let promise = DinoService.getDinos(words, paragraphs);
+  const text = $("#inputtedText").val();
+  const words = Translation.counter(text);
+  let promise = DinoService.getDinos(words);
   promise.then(function(response) {
     const body = JSON.parse(response);
-    $("#showDinos").text(body);
+    console.log(body);
+    $("#showDinos").text(Translation.translator(text, body));
   }, function(error) {
     $(".showErrors").text(`A comet wiped out your dinosaurs. The comet says" ${error}`);
   });
